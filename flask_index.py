eb202a20d9a66e26f5a222f437da6c8be3f1b396
elichyb <elichyb@gmail.com>
2018-04-18 00:15:04 +0300

    Run server successfully

--- flask_index.py
+++ flask_index.py
@@ -1,9 +1,11 @@
 from flask import *
 
+app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello,digigan'
 
 
-app = Flask(__name__)
+app.run(host='localhost', port='8080')
